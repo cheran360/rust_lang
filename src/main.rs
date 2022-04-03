41142c301be5c5ca57ad15c57faebd681bfe9a34
cheran <cherankumar.548@gmail.com>
2022-04-03 13:49:46 +0530

    start of rust

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+// to create file in windows -> "cd . > <filename>"
+// to start a project -> "cargo init"
+// to build and compile the code -> "cargo run"
+// for build in dev -> "cargo build" 
+// for build in production -> "cargo build --release"
+
+fn main() {
+    println!("Hello, world!");
+}
+
+
